24c90be15ef69b798fc21d6bac1838e31d4bb817
Miguel Angel <elmendalerenda@gmail.com>
2013-07-01 20:57:10 +0100

    refactoring items that increase their value

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,31 +13,43 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+var updateBackstagePassesQuality = function(item){
+  if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') return;
+
+  if (item.sell_in <= 10) {
+    item.quality = item.quality + 1
+  }
+
+  if (item.sell_in <= 5) {
+    item.quality = item.quality + 1
+  }
+};
+
+
+var increaseQuality = function(item){
+  if(!isItemThatIncreasesQuality(item)) return;
+  if (item.quality === 50) return;
+
+  item.quality = item.quality + 1
+  updateBackstagePassesQuality(item);
+}
+
+var isItemThatIncreasesQuality = function(item){
+  return (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert');
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (!isItemThatIncreasesQuality(items[i])) {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
         }
       }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
     }
+
+    increaseQuality(items[i]);
+
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
@@ -53,9 +65,7 @@ function update_quality() {
           items[i].quality = items[i].quality - items[i].quality
         }
       } else {
-        if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
-        }
       }
     }
   }
